c89f5e9fb6f396f0418ad9b3e854a6abd3ac7eb8
Oleg Smirnov <osmirnov@gmail.com>
2015-01-02 10:50:50 +0200

    added proper handling of 'nobeam' sources in tigger-restore

--- Tigger/Tools/Imaging.py
+++ Tigger/Tools/Imaging.py
@@ -324,7 +324,7 @@ class ImageResampler (object):
     else:
       return map_coordinates(image,self._target_coords);
 
-def restoreSources (fits_hdu,sources,gmaj,gmin=None,grot=0,freq=None,primary_beam=None,apply_beamgain=False):
+def restoreSources (fits_hdu,sources,gmaj,gmin=None,grot=0,freq=None,primary_beam=None,apply_beamgain=False,ignore_nobeam=False):
   """Restores sources (into the given FITSHDU) using a Gaussian PSF given by gmaj/gmin/grot, in radians.
   gmaj/gmin is major/minor sigma parameter; grot is PA in the North thru East convention (PA=0 is N).
 
@@ -333,6 +333,7 @@ def restoreSources (fits_hdu,sources,gmaj,gmin=None,grot=0,freq=None,primary_bea
   If primary_beam is specified, uses it to apply a PB gain to each source. This must be a function of two arguments:
   r and freq, returning the power beam gain.
   If apply_beamgain is true, applies beamgain atribute instead, if this exists.
+  Source tagged 'nobeam' will not have the PB gain applied, unless ignore_nobeam=True
   """;
   hdr = fits_hdu.header;
   data,stokes,extra_data_axes,dum = getImageCube(fits_hdu);
@@ -392,13 +393,14 @@ def restoreSources (fits_hdu,sources,gmaj,gmin=None,grot=0,freq=None,primary_bea
     else:
       ni = 1;
     #  multiply that by PB gain, if given
-    if apply_beamgain and hasattr(src,'beamgain'):
-      ni *= getattr(src,'beamgain');
-    elif primary_beam:
-      r = getattr(src,'r',None);
-      if r is not None:
-        pb = primary_beam(r,freq);
-        ni *= pb;
+    if ignore_nobeam or not getattr(src,'nobeam',False):
+      if apply_beamgain and hasattr(src,'beamgain'):
+        ni *= getattr(src,'beamgain');
+      elif primary_beam:
+        r = getattr(src,'r',None);
+        if r is not None:
+          pb = primary_beam(r,freq);
+          ni *= pb;
         dprintf(3,"Source %s: r=%g pb=%f, normalized intensity is %f\n",src.name,r,pb,ni);
     # process point sources
     if src.typecode in ('pnt','Gau'):
