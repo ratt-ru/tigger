718510d9c600ed16db5be8f809a6f070124860cf
Gijs Molenaar <gijs@pythonic.nl>
2017-11-13 10:57:54 +0200

    fix #88

--- setup.py
+++ setup.py
@@ -6,20 +6,23 @@ import sys
 from setuptools import setup, find_packages
 import warnings
 
-requirements = ['astro_kittens', 'numpy', 'scipy', 'astlib', 'pyfits']
+msg = """
+Could not detect PyQt4. Install PyQt4 system wide. If you are in
+a virtualenv install the vext.pyqt4 package: 
+
+  $ pip install astro-tigger[venv]
+
+"""
+
 try:
     import PyQt4
 except ImportError:
-    if hasattr(sys, 'real_prefix'):
-        warnings.warn('Could not detect PyQt4, but we detected a virtual environment.'
-                      ' Will proceed with vext.pyqt4. If errors occur, please '
-                      'install PyQt4')
-        requirements.append('vext.pyqt4')
-    else:
-        raise ImportError('PyQt4 not found. Please install it first. If using a virtualenv, you '
-          'may need to install vext.pyqt4')
+    warnings.warn(msg)
 
-__version__ = "1.3.9"
+
+__version__ = "1.3.10"
+
+requirements = ['astro_kittens', 'numpy', 'scipy', 'astlib', 'pyfits']
 
 scripts = [
     'Tigger/bin/tigger-convert',
@@ -34,17 +37,22 @@ package_data = {'Tigger': [
     'tigger.conf',
 ] }
 
+extras_require = {
+    'venv': ['vext.pyqt4'],
+}
+
 
 setup(
-    name = "astro-tigger",
-    version = __version__,
-    packages = find_packages(),
-    scripts = scripts,
-    package_data = package_data,
-    description = "yet another FITS image viewer",
-    author = "Oleg Smirnov",
-    author_email = "osmirnov@gmail.com",
-    url = "https://github.com/ska-sa/tigger",
+    name ="astro-tigger",
+    version=__version__,
+    packages=find_packages(),
+    extras_require=extras_require,
+    scripts=scripts,
+    package_data=package_data,
+    description="yet another FITS image viewer",
+    author="Oleg Smirnov",
+    author_email="osmirnov@gmail.com",
+    url="https://github.com/ska-sa/tigger",
     install_requires=requirements,
 )
 
