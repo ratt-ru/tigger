6d385a916893314461ab2aeeb8730e965b61460c
Raz <razman786@gmail.com>
2021-03-28 12:45:04 +0100

    Locking versions in setup.py for now.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ __version__ = "1.5.0"
 
 requirements = [
     'numpy>=1.17',
-    'scipy>=1.5.2',
+    'scipy==1.5.2',
     'astlib==0.10.2',
-    'astropy==4.2',
+    'astropy==4.1',
     'astro_tigger_lsm==1.7.0',
-    'configparser>=5.0.1',
+    'configparser==5.0.1',
 ]
 
 scripts = [
