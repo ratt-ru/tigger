92e24fdc1da0b5ace6f93ae03d3b9449f410055a
Raz <razman786@gmail.com>
2021-04-13 02:05:31 +0100

    install script tidy

--- install_tigger_ubuntu_20_04.sh
+++ install_tigger_ubuntu_20_04.sh
@@ -3,10 +3,10 @@ echo "Tigger - Ubuntu 20.04 install script"
 tigger_pwd="${PWD}"
 
 # install Tigger deps
-sudo apt install python3-pyqt5.qtsvg python3-pyqt5.qtopengl &&
+sudo apt -y install python3-pyqt5.qtsvg python3-pyqt5.qtopengl &&
 
 # install PyQt-Qwt deps
-sudo apt install pyqt5-dev pyqt5-dev-tools python3-pyqt5 libqwt-qt5-dev libqwt-headers libqt5opengl5-dev &&
+sudo apt -y install pyqt5-dev pyqt5-dev-tools python3-pyqt5 libqwt-qt5-dev libqwt-headers libqt5opengl5-dev git &&
 
 # compile PyQt-Qwt
 cd /tmp &&
