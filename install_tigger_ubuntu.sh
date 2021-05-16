1a7ebf04cc3eb9d167ad47736bf6f51903b52192
Raz <razman786@gmail.com>
2021-05-16 02:02:52 +0100

    Fixed tigger-lsm version detection. Added custom output redirect for log and error files.

--- install_tigger_ubuntu.sh
+++ install_tigger_ubuntu.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
+log_file=tigger_installer.log
+error_file=tigger_installer.err
+
+# custom redirection
+exec 3>&1
+
+# redirect stdout/stderr to a file
+exec >$log_file
+
+# function echo to show echo output on terminal
+echo() {
+   # call actual echo command and redirect output to custom redirect
+   command echo "$@" >&3
+}
+
 echo "==== Tigger v1.6.0 - Ubuntu 20.04 install script ===="
+printf "==== Tigger v1.6.0 - Ubuntu 20.04 install script ====\n"
 
 # install via packages by default
 build_type="package"
@@ -39,23 +56,28 @@ then
 		distro_version=$(lsb_release -rs|sed -e 's/\.//g')
 	else
 		echo "==== Error: Ubuntu Linux not detected, stopping installation ===="
+		printf "==== Error: Ubuntu Linux not detected, stopping installation ====\n"
 		exit 1
 	fi
 else
 	echo "==== Error: Unable to detect Linux distribution, stopping installation ===="
+	printf "==== Error: Unable to detect Linux distribution, stopping installation ====\n"
 	exit 1
 fi
 
-echo "==== Installer has detected Linux distribution as $distro_name $distro_version ==== "
+echo "==== Installer has detected Linux distribution as $distro_name $distro_version ===="
+printf "==== Installer has detected Linux distribution as $distro_name $distro_version ====\n"
 
 # check for pip3 and install if need be
 if command -v pip3 > /dev/null 
 then
 	echo "==== Installer found pip3... ===="
+	printf "==== Installer found pip3... ====\n"
 else
 	echo "==== Installer did not find pip3... ===="
+	printf "==== Installer did not find pip3... ====\n"
 	install_type="fullstack"
-	sudo apt -y install python3-setuptools python3-pip 
+	sudo apt -y install python3-setuptools python3-pip 2>>$error_file
 fi
 
 # check for astro-tigger-lsm
@@ -63,50 +85,60 @@ tigger_lsm=`pip3 list|grep astro-tigger-lsm|awk '{print $1}'`
 if [[ $tigger_lsm == "astro-tigger-lsm" ]]
 then
 	echo "==== Installer found astro-tigger-lsm dependency... ===="
-	tigger_lsm_verison=`pip3 list|grep astro-tigger-lsm|awk '{print $2}'|sed -e 's/\.//g'`
+	printf "==== Installer found astro-tigger-lsm dependency... ====\n"
+	tigger_lsm_version=`pip3 list|grep astro-tigger-lsm|awk '{print $2}'|sed -e 's/\.//g'`
+	
 	if [[ "$tigger_lsm_version" -lt "170" ]]
 	then
 		echo "==== Installer fullstack mode - astro-tigger-lsm version is less than 1.7.0... ===="
+		printf "==== Installer fullstack mode - astro-tigger-lsm version is less than 1.7.0... ====\n"
+		pip3 uninstall -y astro_tigger_lsm 2>>$error_file
 		install_type="fullstack"
 	fi
 else
 	install_type="fullstack"
 	echo "==== Installer fullstack mode - astro-tigger-lsm not found... ===="
+	printf "==== Installer fullstack mode - astro-tigger-lsm not found... ====\n"
 fi	
 
 # install astro-tigger-lsm
 if [[ $install_type == "fullstack" ]]
 then
 	echo "==== Installing Tigger-LSM dependency from source... ===="
-	sudo apt -y install git &&	
+	printf "==== Installing Tigger-LSM dependency from source... ====\n"
+	sudo apt -y install git 2>>$error_file &&
 	cd /tmp &&
 	rm -rf tigger-lsm
-	git clone https://github.com/ska-sa/tigger-lsm.git &&
+	git clone https://github.com/ska-sa/tigger-lsm.git 1>>$log_file 2>>$error_file &&
 	cd tigger-lsm &&
+
 	if [[ $distro_version == "1804" ]]
 	then
-		sudo apt -y install libboost-python-dev casacore* &&
-		pip3 install astropy==4.1
-		pip3 install scipy==1.5.2
+		sudo apt -y install libboost-python-dev casacore* 2>>$error_file &&
+		pip3 install -q astropy==4.1
+		pip3 install -q scipy==1.5.2
 	fi
-	python3 setup.py install --user &&
+
+	python3 setup.py install --user 1>>$log_file 2>>$error_file &&
 	cd /tmp &&
 	cd "${tigger_pwd}"
 fi
 
 echo "==== Installing package dependencies... ===="
+printf "==== Installing package dependencies... ====\n"
 
 # install Tigger deps
-sudo apt -y install python3-pyqt5.qtsvg python3-pyqt5.qtopengl libqwt-qt5-6 &&
+sudo apt -y install python3-pyqt5.qtsvg python3-pyqt5.qtopengl libqwt-qt5-6 2>>$error_file &&
 
 # compile PyQt-Qwt
 if [[ $build_type == "source" ]]
 then
     # install PyQt-Qwt deps
-    sudo apt -y install pyqt5-dev pyqt5-dev-tools python3-pyqt5 libqwt-qt5-dev libqwt-headers libqt5opengl5-dev libqt5svg5-dev g++ dpkg-dev git &&
+    sudo apt -y install pyqt5-dev pyqt5-dev-tools python3-pyqt5 libqwt-qt5-dev libqwt-headers libqt5opengl5-dev libqt5svg5-dev g++ dpkg-dev git 2>>$error_file &&
 	if [[ $distro_version == "2004" ]]
 	then
 		echo "==== Compiling PyQt-Qwt for $distro_name $distro_version... ===="
+		printf "==== Compiling PyQt-Qwt for $distro_name $distro_version... ====\n"
 		cd /tmp &&
 		rm -rf PyQt-Qwt
 		git clone https://github.com/razman786/PyQt-Qwt.git &&
@@ -120,6 +152,7 @@ then
 	elif [[ $distro_version == "1804" ]]
 	then
 		echo "==== Compiling PyQt-Qwt for $distro_name $distro_version... ===="
+		printf "==== Compiling PyQt-Qwt for $distro_name $distro_version... ====\n"
 		cd /tmp &&
 		rm -rf PyQt-Qwt
 		git clone https://github.com/razman786/PyQt-Qwt.git &&
@@ -134,6 +167,7 @@ then
 		cd "${tigger_pwd}"
 	else
 		echo "==== Compiling PyQt-Qwt for $distro_name $distro_version... ===="
+		printf "==== Compiling PyQt-Qwt for $distro_name $distro_version... ====\n"
 		cd /tmp &&
 		rm -rf PyQt-Qwt
 		git clone https://github.com/razman786/PyQt-Qwt.git &&
@@ -152,13 +186,16 @@ then
 	if [[ $distro_version == "2004" ]]
 	then
 		echo "==== Installing PyQwt for $distro_name $distro_version... ===="
+		printf "==== Installing PyQwt for $distro_name $distro_version... ====\n"
 		sudo dpkg -i ubuntu_20_04_deb_pkg/python3-pyqt5.qwt_2.00.00-1build1_amd64.deb
 	elif [[ $distro_version == "1804" ]]
 	then
 		echo "==== Installing PyQwt for $distro_name $distro_version... ===="
+		printf "==== Installing PyQwt for $distro_name $distro_version... ====\n"
 		sudo dpkg -i ubuntu_18_04_deb_pkg/python3-pyqt5.qwt_2.00.00_amd64.deb
 	else
 		echo "==== Error: No PyQt-Qwt package available for $distro_name $distro_version, please try: $0 --source ===="
+		printf "==== Error: No PyQt-Qwt package available for $distro_name $distro_version, please try: $0 --source ====\n"
 		exit 1
 	fi
 fi
@@ -171,24 +208,28 @@ then
     if [[ $python_version == "36" ]]
     then
 		echo "==== Ubuntu 18.04 and Python 3.6 detected, adjusting package versions... ===="
+		printf "==== Ubuntu 18.04 and Python 3.6 detected, adjusting package versions... ====\n"
 
 		# check if Astropy version is already 4.1
 		astropy_verison=`pip3 list|grep astropy|awk '{print $2}'|sed -e 's/\.//g'`
 		if [[ "$astropy_version" -ne "41" ]]
 		then
-			pip3 install astropy==4.1
+			pip3 uninstall -y astropy
+			pip3 install -q astropy==4.1
 		fi
 
 		# check if scipy version is already 1.5.2
 		scipy_verison=`pip3 list|grep scipy|awk '{print $2}'|sed -e 's/\.//g'`
 		if [[ "$scipy_version" -ne "152" ]]
 		then
-			pip3 install scipy==1.5.2
+			pip3 uninstall -y scipy
+			pip3 install -q scipy==1.5.2
 		fi
     fi
 fi
 
 # install Tigger
-python3 setup.py install --user
+python3 setup.py install --user 1>>$log_file 2>>$error_file
 
 echo "==== Tigger installation complete! \o/ ===="
+printf "==== Tigger installation complete! \o/ ====\n"
