52486f1ba6d73d850bdb3a97f7ad59da58b2c91d
Raz <razman786@gmail.com>
2022-05-22 14:21:53 +0100

    Updated compatible dependency versions

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ __version__ = "1.6.1"
 # PyQt (5.15.x) has not been added here are it needs to be installed via apt-get instead to support Qwt.
 # requirements below do not have versions for upstream packaging processes, but tested and compatible versions are given
 requirements = [
-    'numpy',  # tested with version >=1.19.4
-    'scipy',  # tested with versions 1.5.2 for Python 3.6 and >=1.6.2
-    'astlib',  # tested with version 0.11.6
-    'astropy',  # tested with 4.1 and 4.2
+    'numpy',  # tested with version >=1.19.4 (<= 1.22.3)
+    'scipy',  # tested with versions 1.5.2 for Python 3.6 and >=1.6.2 (<= 1.8.0)
+    'astlib',  # tested with version 0.11.6 and 0.11.7
+    'astropy',  # tested with 4.1, 4.2 and 5.0.4
     'astro_tigger_lsm==1.7.1',  # PyQt5 version of astro-tigger-lsm
-    'configparser',  # tested with version 5.0.1
+    'configparser',  # tested with version 5.0.1 and 5.2.0
 ]
 
 scripts = [
