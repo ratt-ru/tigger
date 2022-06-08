9471ab2d7f327f81cc415f224dd60a73fcb9a2c6
Raz <razman786@gmail.com>
2022-06-08 22:20:27 +0100

    Updated tigger-lsm dependency to version 1.7.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
     'scipy',  # tested with versions 1.5.2 for Python 3.6 and >=1.6.2 (<= 1.8.0)
     'astlib',  # tested with version 0.11.6 and 0.11.7
     'astropy',  # tested with 4.1, 4.2 and 5.0.4
-    'astro_tigger_lsm==1.7.1',  # PyQt5 version of astro-tigger-lsm
+    'astro_tigger_lsm==1.7.2',  # PyQt5 version of astro-tigger-lsm
     'configparser',  # tested with version 5.0.1 and 5.2.0
     'reproject',  # tested with version 0.8
 ]
