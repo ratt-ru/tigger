ced7f70af1cae62a395a664d020824ca3d057626
Raz <razman786@gmail.com>
2022-08-04 16:16:04 +0100

    Added copyright notice

--- TigGUI/Plot/PlottableProfiles.py
+++ TigGUI/Plot/PlottableProfiles.py
@@ -1,3 +1,24 @@
+# Copyright (C) 2002-2022
+# The MeqTree Foundation &
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>,
+# or write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 from PyQt5.Qt import QColor, QPen
 from PyQt5.QtCore import Qt
 from PyQt5.Qwt import QwtPlotCurve, QwtPlotItem
